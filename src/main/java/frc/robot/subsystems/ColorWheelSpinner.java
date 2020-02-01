d2955f9cdd0ef0c8f908f5c082af91da3e888b0c
EtomicBomb <ethan@ethan.ws>
2020-02-01 14:18:48 -0800

    code aidan wrote
    Co-authored-by: hellokitty5000 <thedude3015@gmail.com>

--- src/main/java/frc/robot/subsystems/ColorWheelSpinner.java
+++ src/main/java/frc/robot/subsystems/ColorWheelSpinner.java
@@ -19,9 +19,11 @@ import com.revrobotics.ColorMatch;
 
 
 public class ColorWheelSpinner extends SubsystemBase {
-
-    private char gameData;
    
+    private String[] hackyCircularChart = {"R", "G", "B", "Y", "R", "G", "B","Y", "R", "G", "B", "Y"};//start index at 4 end at 8
+    //default is clockwise
+
+    private final Spark spark = new Spark(10);
 
     private final I2C.Port colorSensorPort = I2C.Port.kOnboard;
     //sets up the special port for the color sensor
@@ -32,20 +34,17 @@ public class ColorWheelSpinner extends SubsystemBase {
     private final ColorMatch colorMatcher = new ColorMatch();
     //registers and detects known colors
 
-
-    private final Color red = ColorMatch.makeColor(0.47, 0.37, 0.15);
-    private final Color yellow = ColorMatch.makeColor(0.31, 0.55, 0.13);
-    private final Color green = ColorMatch.makeColor(0.19, 0.55, 0.25);
-    private final Color blue = ColorMatch.makeColor(0.15, 0.44, 0.40);
+    private final Color Red = ColorMatch.makeColor(0.47, 0.37, 0.15);
+    private final Color Green = ColorMatch.makeColor(0.19, 0.55, 0.25);
+    private final Color Blue = ColorMatch.makeColor(0.15, 0.44, 0.40);
+    private final Color Yellow = ColorMatch.makeColor(0.31, 0.55, 0.13);
 
     Color detectedColor;
     ColorMatchResult closestColor;
     //the numbers will be in the range of 0 to 1
 
-    Color[] colorsWeCareAbout = {red ,yellow, green, blue};
+    Color[] colorsWeCareAbout = {Red, Green, Blue, Yellow};
     //group all of the the colors I care about   
-
-    Spark spark;
 
     String colorString;
     //what color the sensor sees in a string
@@ -56,7 +55,7 @@ public class ColorWheelSpinner extends SubsystemBase {
         {
             colorMatcher.addColorMatch(color);//add all of our colors to the black box
         }
-
+    
     }
 
     @Override
@@ -75,48 +74,74 @@ public class ColorWheelSpinner extends SubsystemBase {
         
     }
 
-    public Color targetColor() {
+    public void goToColor() 
+    {
         String gameString = DriverStation.getInstance().getGameSpecificMessage();
-        
-       
-            switch (gameString) {
-                case "":
 
-                break;
-                case"B":
-
-                break;
-                case "R":
-                
-                break;
-                case "Y":
-
-                break;
-            }
-        
+        if(gameString == "" || gameString == colorString)
+        {
+            spark.set(0);
+        }
         else
         {
-            // error
+            int Direction = (leftTurnDistance(gameString)-rightTurnDistance(gameString)) > 0 ? 1: -1;
+            spark.set(Direction);
         }
+        
+    }
+    private int leftTurnDistance(String currentColor)
+    {
+        int startIndex = arrayIndexOf(hackyCircularChart,currentColor) + 4;
+        for(int i = startIndex; i > 0; i--)
+        {
+            if(colorString == hackyCircularChart[i])
+            {
+                return startIndex - i;
+            }
+        }
+        return 0;
+    }
+    private int rightTurnDistance( String current)
+    {
+        int startIndex = arrayIndexOf(hackyCircularChart,current) + 4;
+        for (int i = startIndex; i < hackyCircularChart.length; i++)
+        {
+            if(colorString == hackyCircularChart[i])
+            {
+                return i-startIndex;
+            }
+        }
+        return 0;
+    }
+    private int arrayIndexOf(String[] array, String item)
+    {
+        for (int i = 0; i < array.length; i++) 
+        {
+            if(array[i] == item)
+            {
+                return i;
+            }   
+        }
+        return -1;
     }
 
-    public void toStringColor(ColorMatchResult closestColor)//dont open
+    private void toStringColor(ColorMatchResult closestColor)//dont open
     {
-        if (closestColor.color == blue)
+        if (closestColor.color == Blue)
         {
-            colorString = "Blue";
+            colorString = "B";
         }
-        else if (closestColor.color == red)
+        else if (closestColor.color == Red)
         {
-            colorString = "Red";
+            colorString = "R";
         }
-        else if (closestColor.color == green)
+        else if (closestColor.color == Green)
         {
-            colorString = "Green";
+            colorString = "G";
         } 
-        else if (closestColor.color == yellow) 
+        else if (closestColor.color == Yellow) 
         {
-            colorString = "Yellow";
+            colorString = "Y";
         } 
         else
         {
